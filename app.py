92d0b8b43b03dbe5b1e69d4df6d5481fb56a01f0
Ghani-25 <54942476+Ghani-25@users.noreply.github.com>
2022-08-20 23:23:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ import os
 import math
 import torch
 import pandas as pd
+url = "https://github.com/Ghani-25/waapred/raw/master/BESTmodel_weights.pt"
+output = "BESTmodel_weights.pt"
+gdown.download(url, output, quiet=False)
+
+#model = torch.hub.load_state_dict_from_url('https://github.com/Ghani-25/waapred/blob/db5336462ef64618a84fca6ba4e7224316fe7393/BESTmodel_weights.pt')
+#model.eval()
 with st.sidebar.form("Input"):
     queryText = st.text_area("Response rate to predict:", height=4, max_chars=None)
     btnResult = st.form_submit_button('Run')
@@ -12,21 +18,17 @@ if btnResult:
     st.sidebar.text('Button pushed')
 
     # run query
-    modelfile = "https://drive.google.com/drive/u/3/folders/1EmXO09Yxm9BlPIhgOI5RrXmVHdA0Y3ny"
-    gdown.download_folder(modelfile, quiet=True, use_cookies=False)
-    url = 'model/BESTmodel_weights.pt'
-    if torch.cuda.is_available():       
-        device = torch.device("cuda")
-        print("Using GPU.")
-    else:
-        print("No GPU available, using the CPU instead.")
-        device = torch.device("cpu")
+    #modelfile = "https://drive.google.com/drive/u/3/folders/1EmXO09Yxm9BlPIhgOI5RrXmVHdA0Y3ny"
+    #gdown.download_folder(modelfile, quiet=True, use_cookies=False)
+    #model = torch.hub.load_state_dict_from_url('https://github.com/Ghani-25/waapred/blob/db5336462ef64618a84fca6ba4e7224316fe7393/BESTmodel_weights.pt')
+    #model.eval()
+    urll = './BESTmodel_weights.pt'
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
         
-    model = torch.load(url, map_location=torch.device('cpu'))
+    model = torch.load(urll, map_location=torch.device('cpu'))
     model.to(device)
     model.eval()
-    # model = torch.hub.load_state_dict_from_url('https://github.com/Ghani-25/waapred/blob/db5336462ef64618a84fca6ba4e7224316fe7393/BESTmodel_weights.pt')
-    # model.eval()
+
     from transformers import AutoTokenizer, AutoModelForSequenceClassification, DataCollatorWithPadding
     from torch.utils.data import DataLoader
 
@@ -34,7 +36,7 @@ if btnResult:
     tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL)
 
     y_preds = []
-    encoded = tokenizer(queryText, truncation=True, padding="max_length", max_length=256, return_tensors="pt").to("cuda")
+    encoded = tokenizer(queryText, truncation=True, padding="max_length", max_length=256, return_tensors="pt").to("cpu")
     y_preds += model(**encoded).logits.reshape(-1).tolist()
 
     pd.set_option('display.max_rows', 500)
